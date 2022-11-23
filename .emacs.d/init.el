d0b0a5165e6082bd2318b485b38baeb743d12a6a
Gabriel Simmer <gsimmer@protonmail.com>
2022-11-23 12:04:00 +0000

    Extra emacs packages for writing

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -22,11 +22,11 @@
 ;; straight.el bootstrapping
 (defvar bootstrap-version)
 (let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
       (bootstrap-version 5))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
-        (url-retrieve-synchronously
+       (url-retrieve-synchronously
          "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
          'silent 'inhibit-cookies)
       (goto-char (point-max))
@@ -40,6 +40,8 @@
 (use-package monokai-pro-theme
   :straight t
   :config (load-theme 'monokai-pro-spectrum t))
+(use-package nano-theme
+  :straight t)
 (use-package doom-modeline
   :straight t
   :init (doom-modeline-mode 1)
@@ -126,34 +128,45 @@
   (setq k8s-search-documentation-browser-function 'browse-url-firefox)
   :hook (k8s-mode . yas-minor-mode))
 (use-package kubernetes :straight t)
+(use-package rust-mode :straight t)
 
+(use-package olivetti :straight t)
+(defun me/org-mode ()
+  "My custom configuration for 'org-mode'."
+  (olivetti-mode)
+  (olivetti-set-width 80)
+  )
+(add-hook 'org-mode-hook 'me/org-mode)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(nano))
+ '(custom-safe-themes
+   '("fb83a50c80de36f23aea5919e50e1bccd565ca5bb646af95729dc8c5f926cbf3" "e7820b899036ae7e966dcaaec29fd6b87aef253748b7de09e74fdc54407a7a02" "1781e8bccbd8869472c09b744899ff4174d23e4f7517b8a6c721100288311fa5" default))
  '(safe-local-variable-values
    '((eval modify-syntax-entry 43 "'")
-     (eval modify-syntax-entry 36 "'")
-     (eval modify-syntax-entry 126 "'")
-     (eval let
-	   ((root-dir-unexpanded
-	     (locate-dominating-file default-directory ".dir-locals.el")))
-	   (when root-dir-unexpanded
-	     (let*
-		 ((root-dir
-		   (expand-file-name root-dir-unexpanded))
-		  (root-dir*
-		   (directory-file-name root-dir)))
-	       (unless
-		   (boundp 'geiser-guile-load-path)
-		 (defvar geiser-guile-load-path 'nil))
-	       (make-local-variable 'geiser-guile-load-path)
-	       (require 'cl-lib)
-	       (cl-pushnew root-dir* geiser-guile-load-path :test #'string-equal))))
-     (eval setq-local guix-directory
-	   (locate-dominating-file default-directory ".dir-locals.el"))))
+	 (eval modify-syntax-entry 36 "'")
+	 (eval modify-syntax-entry 126 "'")
+	 (eval let
+		   ((root-dir-unexpanded
+			 (locate-dominating-file default-directory ".dir-locals.el")))
+		   (when root-dir-unexpanded
+			 (let*
+				 ((root-dir
+				   (expand-file-name root-dir-unexpanded))
+				  (root-dir*
+				   (directory-file-name root-dir)))
+			   (unless
+				   (boundp 'geiser-guile-load-path)
+				 (defvar geiser-guile-load-path 'nil))
+			   (make-local-variable 'geiser-guile-load-path)
+			   (require 'cl-lib)
+			   (cl-pushnew root-dir* geiser-guile-load-path :test #'string-equal))))
+	 (eval setq-local guix-directory
+		   (locate-dominating-file default-directory ".dir-locals.el"))))
  '(warning-suppress-log-types '((comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
